9e9a590b1e6b562cc07469ff6bc862196f778579
Chris Malan <16187288@sun.ac.za>
2016-07-17 20:01:41 +0800

    Added online/offline frontend status and other basic GUI elements

--- Client/src/ClientMain/ClientInterface.java
+++ Client/src/ClientMain/ClientInterface.java
@@ -1,14 +1,22 @@
 package ClientMain;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 
 public class ClientInterface extends JFrame {
 	
 	private final int WIDTH = 600;
-	private final int HEIGHT = 400;
+	private final int HEIGHT = 300;
 	
 	public ClientInterface() {
 		
@@ -28,15 +36,33 @@ public class ClientInterface extends JFrame {
 		
 		JPanel mainPanel = new JPanel();
 		JPanel loginpanel = new JPanel();
+		JPanel statusPanel = new JPanel();
 		JPanel chatPanel = new JPanel();
 		
 		JLabel serverLabel = new JLabel("Server IP Address");
 		JLabel portLabel = new JLabel("Port");
 		JLabel usernameLabel = new JLabel("Username");
+		JLabel userStatus = new JLabel();
 		
-		GridLayout mainLayout = new GridLayout(2, 1);
-		GridLayout loginLayout = new GridLayout(2,4);
+		JTextField serverInput = new JTextField(20);
+		JTextField portInput = new JTextField();
+		JTextField usernameInput = new JTextField();
+		
+		JTextArea chatArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(chatArea);
+		
+		GridLayout mainLayout = new GridLayout(3, 1);
+		GridLayout loginLayout = new GridLayout(2,3);
 		GridLayout chatLayout = new GridLayout(1, 0);
+		
+		ImageIcon icon = new ImageIcon("Resources\\offline.png"); 
+		Image image = icon.getImage();
+		Image newImage = image.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+		ImageIcon newImageIcon = new ImageIcon(newImage);
+		userStatus.setIcon(newImageIcon);
+		statusPanel.setOpaque(true);
+		statusPanel.setBackground(Color.WHITE);
+		
 		
 		mainPanel.setLayout(mainLayout);
 		loginpanel.setLayout(loginLayout);
@@ -45,10 +71,16 @@ public class ClientInterface extends JFrame {
 		loginpanel.add(serverLabel);
 		loginpanel.add(portLabel);
 		loginpanel.add(usernameLabel);
+		loginpanel.add(serverInput);
+		loginpanel.add(portInput);
+		loginpanel.add(usernameInput);
 		
+		statusPanel.add(userStatus);
 		
+		chatPanel.add(scrollPane);
 		
 		mainPanel.add(loginpanel);
+		mainPanel.add(statusPanel);
 		mainPanel.add(chatPanel);
 		
 		this.getContentPane().add(mainPanel);
